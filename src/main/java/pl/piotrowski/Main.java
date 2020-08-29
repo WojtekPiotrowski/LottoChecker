2e892bd128462562022d18ce7348196492ba1d82
Wojtek Piotrowski <wojtekpiotrowski94@gmail.com>
2020-08-29 14:49:03 +0200

    JSON reader installed into GameData

--- src/main/java/pl/piotrowski/Main.java
+++ src/main/java/pl/piotrowski/Main.java
@@ -1,8 +1,14 @@
 package pl.piotrowski;
 
-public class Main {
+import com.google.gson.Gson;
 
-    public static void main(String[] args) {
-        System.out.println("Dupa");
-    }
+public class Main {
+        public static void main(String[] args) {
+            Gson gson = new Gson();
+            String json = "{\"imie\": \"Jacek\",\"wiek\": 10, \"adres\": {\"ulica\": \"xxxx\",\"numerDomu\": 1 }, \"dzieci\": null,\"czyMaDzieci\": false,\"tagi\": [\"developer\", \"cos\", \"cos2\"]}";
+            Uzytkownik jacek = gson.fromJson(json, Uzytkownik.class);
+            System.out.println(jacek.imie);
+            System.out.println(jacek.wiek);
+            System.out.println(jacek.adres.ulica);
+        }
 }
